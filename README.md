3227b24a3ac3ff27354743cdbab95251a374ff38
yuranignatenko <yuran.ignatenko@yandex.ru>
2023-02-16 23:28:43 +0300

    load project

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # getstatus
+> Descript
+```
+Console script.
+
+Detect online/offline mode user
+and save history log in/out telegram
+```
+
+Save in file `file_status_<username>.log`
+
+
+***
+
+> Install
+```
+git clone git@github.com:YuranIgnatenko/getstatus.git
+```
+***
+
+> Requirements
+
+```bash
+# python3
+pip3 install telethon
+```
+
+***
+
+> Configure
+```bash
+# path: ~/getstatus/config/config.json
+{
+    "api_hash":"8d7sfsdf7sd8yf8g78s7dfy87gdfy8gv",
+    "api_id":"998877",
+    "delay_sec":0
+}
+```
+
+***
+
+> First Launch
+
+```bash
+cd ~/getstatus/
+python3 getstatus.py test 1
+# Enter phone-number
+# Wait Check-message code
+# Enter code
+```
+
+> Examples:
+``` bash
+# using:
+python3 getstatus.py [target] [mode-count]
+
+# set mode script:
+python3 getstatus.py Bob 1 #1 lines to file
+python3 getstatus.py Bob 3 #3 lines to file
+python3 getstatus.py Bob 0 #while lines to file
+
+# set target-user:
+python3 getstatus.py Alice 1
+python3 getstatus.py @Alice_998877 1
+python3 getstatus.py +79008007060 1
+python3 getstatus.py 89008007060 1
---
+++ getstatus.py
@@ -0,0 +1,91 @@
+from telethon.sync import TelegramClient
+import os, sys, json, time
+
+
+target_user = sys.argv[1]
+count_scan = sys.argv[2]
+session_file = "newSessionStatus"
+symsplit = "&"
+config_file = 'config/config.json'
+prefix = "file_status_"
+postfix = ".log"
+
+
+class Bot():
+    def __init__(self, api_hash, api_id, namefilelog, phone, delay_sec):
+        self.ErrorFindUser = "UserNotFound"
+        self.api_hash = api_hash 
+        self.api_id = api_id
+        self.namefile = namefilelog
+        self.target_user = phone
+        self.status_user = ""
+        self.delay_sec = delay_sec
+        self.symsplit = symsplit
+        self.client = TelegramClient(session_file, api_id, api_hash)
+        self.client.start()
+        # warn init line
+        for dialog in self.client.iter_dialogs():
+            break
+        self.check_file()
+
+    def check_file(self):
+        if os.path.isfile(self.namefile) == False:
+            with open(self.namefile, "w+") as file:
+                file.read() 
+
+    def get_status(self, user_name):
+        try:
+            acc = str(self.client.get_entity(user_name).status).split("(")[0]
+            return f"{acc}"
+        except Exception as e:
+            print(e)
+            return f"{self.ErrorFindUser}"
+
+    def write_file_line(self, status):
+        with open(self.namefile, "a") as file:
+            file.write(time.asctime() + self.symsplit + status + "\n")
+
+    def old_status_from_file(self):
+        with open(self.namefile, "r") as file:
+            data = file.read().strip()
+            if data == "":
+                return self. ErrorFindUser
+            data = data.split("\n")[-1]
+            data = data.split(self.symsplit)[-1]
+            return data
+        
+    def run(self):
+        result = self.get_status(self.target_user)
+        if self.old_status_from_file() != result:
+            self.write_file_line(result)
+
+
+
+def validate_flags():
+    if len(sys.argv) != 3:	
+        if len(sys.argv) == 1:
+            print("Not found name: username, @username, +7999.. , 8999....")
+            sys.exit(0)
+        if len(sys.argv) == 2:
+            print("Not found count-scans: (any int) or (0-while loop)")
+            sys.exit(0)
+
+    
+def main():
+    validate_flags()
+
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+
+    namefile = f"{prefix}{target_user}{postfix}"
+    b = Bot(config["api_hash"], config["api_id"], namefile, target_user, config["delay_sec"])
+
+    if int(count_scan) == 0:
+        while True:
+            b.run()
+    else:
+        for i in range(int(count_scan)):
+            b.run()
+
+
+main()
